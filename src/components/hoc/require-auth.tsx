762444cd9396382baa472b810ccfe7d1a7b314e5
Thiago Andrade <thiagsou@amazon.com>
2017-12-27 21:17:36 -0500

    Add access to Router history to push locations back and forth

--- src/components/hoc/require-auth.tsx
+++ src/components/hoc/require-auth.tsx
@@ -1,13 +1,40 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Component, ComponentClass } from 'react';
+import * as PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter, match } from 'react-router';
+import { History, Location } from 'history';
+
 import * as createReactClass from 'create-react-class';
 
 export const requireAuthComponentClass = (ComposedComponent): ComponentClass => {
   const RequireAuth = createReactClass({
+    propTypes: {
+      match: PropTypes.object.isRequired,
+      location: PropTypes.object.isRequired,
+      history: PropTypes.object.isRequired,
+    },
+    checkAuthentication: function(history: History) {
+      console.log('is authenticated', this.props.authenticated);
+      console.log('history', history);
+      if (!this.props.authenticated) {
+        history.push('/');
+      }
+    },
+    componentDidUpdate: function(prevProps: Readonly<{}>, prevState: Readonly<{}>, prevContext: any) {
+      const history: History = this.props.history;
+      this.checkAuthentication(history);
+    },
+    componentWillMount: function() {
+      const match: match<{}> = this.props.match;
+      const location: Location = this.props.location;
+      console.log('match,', match);
+      console.log('location, ', location);
+      const history: History = this.props.history;
+      this.checkAuthentication(history);
+    },
     render: function() {
-      console.log(this.props.authenticated);
       return <ComposedComponent {...this.props} />;
     },
   });
@@ -16,7 +43,7 @@ export const requireAuthComponentClass = (ComposedComponent): ComponentClass => 
     return { authenticated: state.authenticated };
   };
 
-  return connect(mapStateToProps, null)(RequireAuth);
+  return withRouter(connect(mapStateToProps, null)(RequireAuth));
 };
 
 export const requireAuthComponentElement = (ComposedComponent): JSX.Element => {
